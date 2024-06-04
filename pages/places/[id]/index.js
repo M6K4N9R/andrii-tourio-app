43d3dbaeba7c029d7620ba88350b1d4fbe4f2b22
M6K4N9R <zubchevskiy@gmail.com>
2024-06-04 22:00:46 +0200

    READ data works. On to CREATE FORM

--- pages/places/[id]/index.js
+++ pages/places/[id]/index.js
@@ -33,15 +33,15 @@ export default function DetailsPage() {
   const router = useRouter();
   const { isReady } = router;
   const { id } = router.query;
-  const {
-    data: { place, comments } = {},
-    isLoading,
-    error,
-  } = useSWR(`/api/places/${id}`);
+  const { data, isLoading, error } = useSWR(`/api/places/${id}`);
 
-  console.log("Data on details page: ", id);
+  
 
   if (!isReady || isLoading || error) return <h2>Loading...</h2>;
+
+  const { name, location, comments, mapURL, description } = data || {};
+
+  if (!data) return <h2>Place not found</h2>;
 
   function deletePlace() {
     console.log("deleted?");
@@ -54,7 +54,7 @@ export default function DetailsPage() {
       </Link>
       <ImageContainer>
         <StyledImage
-          src={place.image}
+          src={data.image}
           priority
           fill
           sizes="(max-width: 768px) 100vw,
@@ -64,12 +64,12 @@ export default function DetailsPage() {
         />
       </ImageContainer>
       <h2>
-        {place.name}, {place.location}
+        {name}, {location}
       </h2>
-      <Link href={place.mapURL} passHref legacyBehavior>
+      <Link href={mapURL} passHref legacyBehavior>
         <StyledLocationLink>Location on Google Maps</StyledLocationLink>
       </Link>
-      <p>{place.description}</p>
+      <p>{description}</p>
       <ButtonContainer>
         <Link href={`/places/${id}/edit`} passHref legacyBehavior>
           <StyledLink>Edit</StyledLink>
@@ -78,7 +78,7 @@ export default function DetailsPage() {
           Delete
         </StyledButton>
       </ButtonContainer>
-      <Comments locationName={place.name} comments={comments} />
+      <Comments locationName={name} comments={comments} />
     </>
   );
 }
